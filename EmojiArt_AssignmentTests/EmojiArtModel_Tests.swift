fc04753c05ccd83364b748144bdf23380ce55243
dhkim <dh5437@naver.com>
2025-02-28 15:06:38 +0900

    FEAT: - Test code added

--- EmojiArt_AssignmentTests/EmojiArtModel_Tests.swift
+++ EmojiArt_AssignmentTests/EmojiArtModel_Tests.swift
@@ -53,6 +53,115 @@ struct EmojiArtModel_Tests {
         #expect(model.emojis.count == randomCount)
     }
     
+    @MainActor
+    @Test mutating func test_EmojiArtModel_selectEmoji_shouldSelectEmoji() async throws {
+        // Given
+        model.addEmoji("😄", at: Emoji.Position(x: 0, y: 0), size: 10)
+        
+        // When
+        model.selectEmoji(with: model.emojis[0])
+        
+        // Then
+        #expect(model.emojis[0].isSelected)
+    }
+    
+    @MainActor
+    @Test mutating func test_EmojiArtModel_selectEmoji_shouldDeselectEmoji() async throws {
+        // Given
+        model.addEmoji("😄", at: Emoji.Position(x: 0, y: 0), size: 10)
+        
+        // When
+        model.selectEmoji(with: model.emojis[0])
+        #expect(model.emojis[0].isSelected)
+        model.selectEmoji(with: model.emojis[0])
+        
+        // Then
+        #expect(!model.emojis[0].isSelected)
+    }
+    
+    @MainActor
+    @Test mutating func test_EmojiArtModel_deselectAllEmoji_shouldDeselectAllEmoji() async throws {
+        // Given
+        model.addEmoji("😄", at: Emoji.Position(x: 0, y: 0), size: 10)
+        model.addEmoji("❤️", at: Emoji.Position(x: 1, y: 1), size: 10)
+        // When
+        #expect(model.emojis.count == 2)
+        
+        model.selectEmoji(with: model.emojis[0])
+        model.selectEmoji(with: model.emojis[1])
+        #expect(model.emojis[0].isSelected)
+        #expect(model.emojis[1].isSelected)
+        model.deselectAllEmojis()
+        // Then
+        #expect(!model.emojis[0].isSelected)
+    }
+    
+    @MainActor
+    @Test mutating func test_EmojiArtModel_deselectAllEmoji_shouldDeselectAllEmoji_stress() async throws {
+        // Given
+        let randomCount = Int.random(in: 10...100)
+        // When
+        for _ in 0..<randomCount {
+            model.addEmoji(
+                "😄",
+                at: Emoji.Position(
+                    x: Int.random(in: -100...100),
+                    y: Int.random(in: -100...100)
+                ),
+                size: 10)
+            model.selectEmoji(with: model.emojis.last!)
+        }
+        #expect(model.emojis.count == randomCount)
+        #expect(model.emojis.allSatisfy({ emoji in
+            emoji.isSelected == true
+        }))
+        
+        model.deselectAllEmojis()
+        // Then
+        #expect(model.emojis.allSatisfy({ emoji in
+            emoji.isSelected == false
+        }))
+    }
+    
+    @MainActor
+    @Test mutating func test_EmojiArtModel_deleteEmoji_shouldDeleteEmoji() async throws {
+        // Given
+        model.addEmoji("😄", at: Emoji.Position(x: 0, y: 0), size: 10)
+        
+        // When
+        #expect(model.emojis.count == 1)
+        
+        model.deleteEmoji(with: model.emojis[0])
+        
+        // Then
+        #expect(model.emojis.isEmpty)
+    }
+    
+    @MainActor
+    @Test mutating func test_EmojiArtModel_deleteEmoji_shouldDeleteEmoji_stress() async throws {
+        // Given
+        let randomCount = Int.random(in: 0..<1000)
+        // When
+        for _ in 0..<randomCount {
+            model.addEmoji(emojis.map { String($0) }.randomElement()!,
+                           at: Emoji.Position(
+                            x: Int.random(in: -1000..<1000),
+                            y: Int.random(in: -1000..<1000)
+                           ),
+                           size: Int.random(in: 0..<30)
+            )
+        }
+        #expect(model.emojis.count == randomCount)
+        
+        for _ in 0..<randomCount {
+            if let randomEmoji = model.emojis.randomElement() {
+                model.deleteEmoji(with: randomEmoji)
+            }
+        }
+        
+        // Then
+        #expect(model.emojis.isEmpty)
+    }
 }
 
 
